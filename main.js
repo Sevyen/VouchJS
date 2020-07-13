0920b31abf8b5f3d8e13491ffd215bcda050cc85
Sevyen <68214584+Sevyen@users.noreply.github.com>
2020-07-12 21:39:52 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+const Discord = require('discord.js');
+
+const client = new Discord.Client();
+
+
+
+client.once('ready', () => {
+    console.log('VouchJS is online!');
+});
+
+
+
+client.login('NzE3OTgwMTk4NDE0NTgxNzcw.XtmWCA.ZAoxJyL0UH0ne_DDlVcifQAgq4c')
